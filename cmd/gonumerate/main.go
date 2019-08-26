e189f234c1d2fb09857f19abf865f66843e999c4
Nick Mrozowski <nickmro@gmail.com>
2019-08-25 22:31:18 -0700

    Decrease complexity

--- cmd/gonumerate/main.go
+++ cmd/gonumerate/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"flag"
 	"fmt"
 	"go/build"
@@ -25,17 +26,20 @@ Options:
  -help        Print usage
 `
 
-func main() {
-	typeOpt := flag.String("type", "", "The enum type name (Required)")
-	descOpt := flag.String("description", "", "The type comment description (must be format \"<type> is...\")")
-	valuesOpt := flag.String("values", "", "The comma-separated list of enum values")
-	prefixOpt := flag.String("prefix", "", "The prefix to apply to each enum value")
-	jsonOpt := flag.String("json", "", "The JSON encoding type {string, int}")
-	sqlOpt := flag.String("sql", "", "The SQL encoding type {string, int}")
-	outputOpt := flag.String("o", "", "The output filename")
-	printOpt := flag.Bool("help", false, "Print usage")
+var (
+	typeOpt   = flag.String("type", "", "The enum type name (Required)")
+	descOpt   = flag.String("description", "", "The type comment description (must be format \"<type> is...\")")
+	valuesOpt = flag.String("values", "", "The comma-separated list of enum values")
+	prefixOpt = flag.String("prefix", "", "The prefix to apply to each enum value")
+	jsonOpt   = flag.String("json", "", "The JSON encoding type {string, int}")
+	sqlOpt    = flag.String("sql", "", "The SQL encoding type {string, int}")
+	outputOpt = flag.String("o", "", "The output filename")
+	printOpt  = flag.Bool("help", false, "Print usage")
+)
 
+func main() {
 	var e gonumerate.Enum
+	var err error
 
 	if flag.Parse(); !flag.Parsed() {
 		fmt.Println(usageText)
@@ -47,63 +51,38 @@ func main() {
 		os.Exit(1)
 	}
 
-	if typeOpt == nil || *typeOpt == "" {
-		fmt.Println(usageText)
-		os.Exit(1)
-	}
-
-	e.Type = *typeOpt
-
-	if descOpt != nil && *descOpt != "" {
-		e.Description = *descOpt
-	}
-
-	if valuesOpt != nil && *valuesOpt != "" {
-		e.Values = strings.Split(*valuesOpt, ",")
-	}
-
-	if prefixOpt != nil {
-		e.Prefix = *prefixOpt
-	}
-
-	if jsonOpt != nil && *jsonOpt != "" {
-		if j := gonumerate.EncodingFromString(*jsonOpt); j != 0 {
-			e.JSONEncoding = j
-		} else {
-			fmt.Println(usageText)
-			os.Exit(1)
-		}
-	}
-
-	if sqlOpt != nil && *sqlOpt != "" {
-		if s := gonumerate.EncodingFromString(*sqlOpt); s != 0 {
-			e.SQLEncoding = s
-		} else {
-			fmt.Println(usageText)
-			os.Exit(1)
-		}
-	}
-
-	if outputOpt != nil && *outputOpt != "" {
-		e.OutFile = *outputOpt
-	}
-
-	wd, err := os.Getwd()
+	e.Type, err = parseType()
 	if err != nil {
-		fmt.Println(err)
+		printError(err)
 		os.Exit(1)
 	}
 
-	p, err := getPackageName(wd)
+	e.JSONEncoding, err = parseJSON()
 	if err != nil {
-		fmt.Println(err)
+		printError(err)
 		os.Exit(1)
 	}
-	e.Package = p
+
+	e.SQLEncoding, err = parseSQL()
+	if err != nil {
+		printError(err)
+		os.Exit(1)
+	}
+
+	e.Package, err = parsePackageName()
+	if err != nil {
+		printError(err)
+		os.Exit(1)
+	}
+
+	e.Description = parseDescription()
+	e.Values = parseValues()
+	e.Prefix = parsePrefix()
+	e.OutFile = praseOutFile()
 
 	f, err := os.Create(e.FileName())
 	if err != nil {
-		fmt.Println(err)
+		printError(err)
 		os.Exit(1)
 	}
 	defer f.Close()
@@ -113,19 +92,86 @@ func main() {
 
 	err = e.Write(w)
 	if err != nil {
-		fmt.Println("Error: ", err)
-		fmt.Println()
-		fmt.Println(usageText)
+		printError(err)
 		os.Remove(e.FileName())
 		os.Exit(1)
 	}
 }
 
-func getPackageName(dir string) (string, error) {
-	pkg, err := build.ImportDir(dir, 0)
+func parseType() (string, error) {
+	if typeOpt == nil || *typeOpt == "" {
+		return "", errors.New("type required")
+	}
+	return *typeOpt, nil
+}
+
+func parseDescription() string {
+	if descOpt != nil && *descOpt != "" {
+		return *descOpt
+	}
+	return ""
+}
+
+func parseValues() []string {
+	if valuesOpt != nil && *valuesOpt != "" {
+		return strings.Split(*valuesOpt, ",")
+	}
+	return []string{}
+}
+
+func parsePrefix() string {
+	if prefixOpt != nil {
+		return *prefixOpt
+	}
+	return ""
+}
+
+func parseJSON() (gonumerate.Encoding, error) {
+	if jsonOpt != nil && *jsonOpt != "" {
+		if j := gonumerate.EncodingFromString(*jsonOpt); j != 0 {
+			return j, nil
+		} else {
+			return 0, errors.New("invalid json value")
+		}
+	}
+	return 0, nil
+}
+
+func parseSQL() (gonumerate.Encoding, error) {
+	if sqlOpt != nil && *sqlOpt != "" {
+		if s := gonumerate.EncodingFromString(*sqlOpt); s != 0 {
+			return s, nil
+		} else {
+			return 0, errors.New("invalid sql value")
+		}
+	}
+	return 0, nil
+}
+
+func praseOutFile() string {
+	if outputOpt != nil && *outputOpt != "" {
+		return *outputOpt
+	}
+	return ""
+}
+
+func parsePackageName() (string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	pkg, err := build.ImportDir(wd, 0)
 	if err != nil {
 		return "", err
 	}
 
 	return pkg.Name, nil
+}
+
+func printError(e error) {
+	fmt.Println("Error:", e)
+	fmt.Println()
+	fmt.Println(usageText)
 }
